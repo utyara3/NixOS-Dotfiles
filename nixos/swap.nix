a4f2805ebacf15faab9427da1a5f8ef6f8c542d3
utyara3 <prorok2018pro@gmail.com>
2026-07-23 16:24:28 +0300

    commit

--- nixos/swap.nix
+++ nixos/swap.nix
@@ -1,16 +1,17 @@
 # nixos/swap.nix
 
-{ config, pkgs, ...}:
+{ ... }:
 
 {
-  
   zramSwap = {
     enable = true;
     memoryPercent = 50;
   };
 
-  swapDevices = [ {
-    device = "/var/lib/swapfile";
-    size = 24 * 1024;
-  } ];
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 24 * 1024;
+    }
+  ];
 }
