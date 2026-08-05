dd7d03d2ace16653b116a821f974890286ce5dfc
utyara3 <prorok2018pro@gmail.com>
2026-08-05 17:10:13 +0300

    nixos-rebuild: generation 132 (2026-08-05 17:10)

--- home/modules/foot.nix
+++ home/modules/foot.nix
@@ -5,15 +5,12 @@
 {
   programs.foot = {
     enable = true;
+    server.enable = true;
 
     settings = {
       main = {
         font = "Fira Code Nerd Font:size=12";
-
         initial-window-size-chars = "120x35";
-        # initial-window-width = "1200";
-        # initial-window-height = "750";
-
         pad = "0x0";
         term = "xterm-256color";
       };
@@ -32,13 +29,12 @@
         hide-when-typing = "yes";
       };
 
-      colors = {
-        # Прозрачность фона в процентах
-        # Формат: AARRGGBB (Alpha, Red, Green, Blue)
-        # a6 в начале задает ~65% непрозрачности (0.65). 000000 - черный цвет фона.
-        background = "a6000000";
+      # Прозрачность терминала
+      "colors-dark" = {
+        # Задаем прозрачность через alpha
+        alpha = "0.65";
 
-        # Цвет текста (белый по умолчанию, можете заменить на свой)
+        background = "000000";
         foreground = "ffffff";
       };
     };
