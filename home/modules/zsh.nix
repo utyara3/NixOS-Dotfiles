e095a89f876728999a3809bfc9a38030d90ed19e
utyara3 <prorok2018pro@gmail.com>
2026-08-05 17:56:40 +0300

    nixos-rebuild: generation 142 (2026-08-05 17:56)

--- home/modules/zsh.nix
+++ home/modules/zsh.nix
@@ -1,6 +1,6 @@
+# home/modules/zsh.nix
 {
   pkgs,
-  lib,
   ...
 }:
 
@@ -9,11 +9,9 @@
     enable = true;
     enableCompletion = true;
 
-    # Возвращаем рабочий синтаксис Home Manager для автодополнений
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
-    # Системные переменные окружения для пользователя
     sessionVariables = {
       EDITOR = "nvim";
       VISUAL = "nvim";
@@ -21,7 +19,6 @@
       LC_ALL = "ru_RU.UTF-8";
     };
 
-    # Настройки истории команд
     history = {
       size = 50000;
       save = 50000;
@@ -38,22 +35,15 @@
       "HIST_REDUCE_BLANKS"
     ];
 
-    # Сюда вы сможете добавлять свои алиасы (шаблон)
     shellAliases = {
-      ls = "eza --icons --group-directories-first --git";
-      ll = "eza --long --icons --group-directories-first --git";
-      la = "eza --all --icons --group-directories-first --git";
-      tree = "eza --tree --icons --group-directories-first --git";
       ssrpg = "cd '/home/utyara3/.local/share/Steam/steamapps/common/Stone Story RPG/Martian Rex, Inc_/Stone Story/76561198016524242/Stonescript/utyara3'";
       vact = "source .venv/bin/activate";
       hist = "history -i | grep $(date +%F)";
       clc = "fn -ln -1 | wl-copy";
       notes = "nvim ~/Documents/helper/";
-      rebuild = "sudo nixos-rebuild switch --flake .";
       n = "nvim";
     };
 
-    # Настройка Oh My Zsh
     oh-my-zsh = {
       enable = true;
       plugins = [
@@ -64,7 +54,6 @@
       ];
     };
 
-    # Сторонние плагины и темы
     plugins = [
       {
         name = "powerlevel10k";
@@ -78,31 +67,53 @@
       }
     ];
 
-    # Используем правильную склейку строк для initContent через стандартный mkMerge
-    initContent = lib.mkMerge [
-      (lib.mkBefore ''
-        # Instant Prompt для p10k
-        if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-          source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-        fi
-      '')
-      ''
-        # Код, который выполнится следом (бывший initExtra)
+    initExtraFirst = ''
+      # Instant Prompt для p10k
+      if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+        source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+      fi
+    '';
 
-        # Бинд клавиш
-        bindkey '^H' backward-kill-word
+    initExtra = ''
+      # Бинд клавиш
+      bindkey '^H' backward-kill-word
 
-        # Direnv
-        eval "$(direnv hook zsh)"
+      # Direnv
+      eval "$(direnv hook zsh)"
 
-        # Автодополнение для uvx (если пакет uv установлен)
-        if command -v uvx &> /dev/null; then
-          eval "$(uvx --generate-shell-completion zsh)"
-        fi
+      # Автодополнение для uvx (если пакет uv установлен)
+      if command -v uvx &> /dev/null; then
+        eval "$(uvx --generate-shell-completion zsh)"
+      fi
 
-        # Загрузка кастомных настроек p10k (если файл существует)
-        [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-      ''
+      # Загрузка кастомных настроек p10k (если файл существует)
+      [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+    '';
+  };
+
+  # Прокачка CLI
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.bat = {
+    enable = true;
+    config.theme = "Visual Studio Dark+";
+  };
+
+  programs.eza = {
+    enable = true;
+    enableZshIntegration = true;
+    icons = "auto";
+    extraOptions = [
+      "--group-directories-first"
+      "--header"
     ];
   };
 }
